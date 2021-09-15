f2d4500726ab41007d5f591eef59d68c2c6f3a94
JulienT91 <julien.tavet30@gmail.com>
2021-09-15 22:00:28 +0200

    add all my project from my other computer

--- src/components/CardEvent.jsx
+++ src/components/CardEvent.jsx
@@ -1,39 +1,41 @@
 import React from "react";
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser";
 import DateFunction from "../services/DateFunction";
 import FavBtn from "./FavBtn";
 import { Link } from "react-router-dom";
 
-
-
-function CardEvent({event,id}) {
-const eventDate = event.date_start;
-return(
-<div className="card-content">
-        <figure>
-            <figcaption>
-            <Link to={{ pathname: "/details", search: `${id}` }}>
-                    <h3 className="title-small">{event.title}</h3>
-            </Link>
-            </figcaption>
-            <div className="zoom_img">
-                <img src={event.cover_url} alt={event.cover_alt} className="card-img-transition" />
-            </div>
-        </figure>
-        <div className="card-desc">
-            <div className="media">
-                <div className="media-content">
-                        <p>{DateFunction.DateForm(eventDate)}</p>
-                </div>
-            </div>
-            <div className="content-desc">
-                {ReactHtmlParser(event.lead_text)}
-                <br />
-                <FavBtn />
-            </div>
-     </div>
-</div>
-    );
+function CardEvent({ event, id }) {
+  const eventDate = event.date_start;
+  return (
+    <div className="card-content">
+      <figure>
+        <figcaption>
+          <Link to={{ pathname: "/details", search: `${id}` }}>
+            <h3 className="title-small">{event.title}</h3>
+          </Link>
+        </figcaption>
+        <div className="zoom_img">
+          <img
+            src={event.cover_url}
+            alt={event.cover_alt}
+            className="card-img-transition"
+          />
+        </div>
+      </figure>
+      <div className="card-desc">
+        <div className="media">
+          <div className="media-content">
+            <p>{DateFunction.DateForm(eventDate)}</p>
+          </div>
+        </div>
+        <div className="content-desc">
+          {ReactHtmlParser(event.lead_text)}
+          <br />
+          <FavBtn id={id} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CardEvent;
--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -1,84 +1,100 @@
-import React ,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import FavBtn from "../components/FavBtn";
-import ReactHtmlParser from 'react-html-parser';
-import {ImPhone} from "react-icons/im";
-import {HiMail} from "react-icons/hi";
-import {CgWebsite} from "react-icons/cg";
-
-
+import ReactHtmlParser from "react-html-parser";
+import { ImPhone } from "react-icons/im";
+import { HiMail } from "react-icons/hi";
+import { CgWebsite } from "react-icons/cg";
 
 const Details = () => {
-
-    const id = useLocation().search.split("?")[1];
-    const[EventDetails,setEventDetails] = useState(null);
-    useEffect (() => {
-        detailsResults();
-    },[]) 
-     const detailsResults = () => {
-        fetch(`https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records/${id}`)
-         .then((result) => result.json())
-         .then((response) => {
-         setEventDetails(response.record.fields)
-         });
-    }
-
-    return(
-        <div className="Details-home">
-            <div className="Details-container">
-             {EventDetails &&
-             <>
-                <div className="details-card">
-                    <div className="details_desc">
-                    <h2>{EventDetails.title}</h2>
-                        <figure>
-                        <img src={EventDetails.cover_url} alt={EventDetails.cover_alt} />
-                            <figcaption>
-                                <h3>{ReactHtmlParser(EventDetails.lead_text)}</h3>
-                                {ReactHtmlParser(EventDetails.description)}
-                            </figcaption>
-                        </figure>
-                    </div>  
-                    <div className="details_info">
-                    <div className="FavDiv">
-                                <span>Ajouter aux favoris</span>
-                                <FavBtn />
-                            </div>
-                        <div>
-                            <h3>Dates:</h3>
-                            <p>{ReactHtmlParser(EventDetails.date_description)}</p>
-                        </div>
-                        <div>
-                            <h3>Prix :</h3>
-                            <p>{EventDetails.price_detail}</p>
-                        </div>
-                        <div>
-                            <h3>S'y rendre : </h3>
-                            <address>
-
-                                <p>{EventDetails.address_name}-{EventDetails.contact_name}</p>
-                                <p>{EventDetails.address_street}-{EventDetails.address_zipcode} {EventDetails.address_city}</p>
-                                
-                            </address>
-                        </div>
-                        <div>
-                            <h3>En transport :</h3>
-                            <p>{ReactHtmlParser(EventDetails.transport)}</p>
-                        </div>
-                        <div>
-                            <h3>Plus d'info</h3>
-                            <p><ImPhone/> : {EventDetails.access_phone}</p>
-                            <p><HiMail/> : <a href="mailto:{EventDetails.access_mail}">{EventDetails.access_mail}</a></p>
-                            <p><CgWebsite/> :<a target="_blank" href={EventDetails.contact_url}>{EventDetails.contact_url}</a></p>
-                        </div>
-                    </div>
-                </div>
-             </>
-             }
-            </div>
-        </div>
+  const id = useLocation().search.split("?")[1];
+  const [EventDetails, setEventDetails] = useState(null);
+  useEffect(() => {
+    detailsResults();
+  }, []);
+  const detailsResults = () => {
+    fetch(
+      `https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records/${id}`
     )
-}
+      .then((result) => result.json())
+      .then((response) => {
+        setEventDetails(response.record.fields);
+      });
+  };
 
+  return (
+    <div className="Details-home">
+      <div className="Details-container">
+        {EventDetails && (
+          <>
+            <div className="details-card">
+              <div className="details_desc">
+                <h2>{EventDetails.title}</h2>
+                <figure>
+                  <img
+                    src={EventDetails.cover_url}
+                    alt={EventDetails.cover_alt}
+                  />
+                  <figcaption>
+                    <h3>{ReactHtmlParser(EventDetails.lead_text)}</h3>
+                    {ReactHtmlParser(EventDetails.description)}
+                  </figcaption>
+                </figure>
+              </div>
+              <div className="details_info">
+                <div className="FavDiv">
+                  <span>Ajouter aux favoris</span>
+                  <FavBtn id={id} w />
+                </div>
+                <div>
+                  <h3>Dates:</h3>
+                  <p>{ReactHtmlParser(EventDetails.date_description)}</p>
+                </div>
+                <div>
+                  <h3>Prix :</h3>
+                  <p>{EventDetails.price_detail}</p>
+                </div>
+                <div>
+                  <h3>S'y rendre : </h3>
+                  <address>
+                    <p>
+                      {EventDetails.address_name}-{EventDetails.contact_name}
+                    </p>
+                    <p>
+                      {EventDetails.address_street}-
+                      {EventDetails.address_zipcode} {EventDetails.address_city}
+                    </p>
+                  </address>
+                </div>
+                <div>
+                  <h3>En transport :</h3>
+                  <p>{ReactHtmlParser(EventDetails.transport)}</p>
+                </div>
+                <div>
+                  <h3>Plus d'info</h3>
+                  <p>
+                    <ImPhone /> : {EventDetails.access_phone}
+                  </p>
+                  <p>
+                    <HiMail /> :{" "}
+                    <a href="mailto:{EventDetails.access_mail}">
+                      {EventDetails.access_mail}
+                    </a>
+                  </p>
+                  <p>
+                    <CgWebsite /> :
+                    <a target="_blank" href={EventDetails.contact_url}>
+                      {EventDetails.contact_url}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Details;
