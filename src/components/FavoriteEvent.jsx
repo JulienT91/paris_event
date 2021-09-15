88cfc18e817dc91edb14259a5384b5d624a033e9
JulienT91 <julien.tavet30@gmail.com>
2021-09-15 22:35:00 +0200

    add css and create content for Favorites Event

--- src/components/FavoriteEvent.jsx
+++ src/components/FavoriteEvent.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import ReactHtmlParser from "react-html-parser";
+import { Link } from "react-router-dom";
 
 const FavoriteEvent = ({ id }) => {
   const [EventDetails, setEventDetails] = useState(null);
@@ -18,7 +20,24 @@ const FavoriteEvent = ({ id }) => {
 
   if (!EventDetails) return <p>Loading ...</p>;
 
-  return <p>{EventDetails.title}</p>;
+  return <div className="favorites-card">
+      <Link to={{ pathname: "/details", search: `${id}` }}>
+            <h3 className="title-small">{EventDetails.title}</h3>
+        </Link>
+            <figure>
+              <div className="zoom_img">
+                <img 
+                      className="card-img-transition"
+                      src={EventDetails.cover_url}
+                      alt={EventDetails.cover_alt} 
+                />
+              </div>
+              <figcaption>
+                <h4>{ReactHtmlParser(EventDetails.lead_text)}</h4>
+              </figcaption>
+            </figure>
+            <p className="details-link"> <Link to={{ pathname: "/details", search: `${id}` }}>En savoir plus...</Link></p>
+      </div>
 };
 
 export default FavoriteEvent;
